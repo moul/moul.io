a8df3f33554c92be26d78c5e1944772f20c28034
Manfred Touron <94029+moul@users.noreply.github.com>
2022-04-07 14:51:23 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -31,6 +31,13 @@ EOF
     <meta property="og:image" content="https://$domain/moul-codes.png">
     <meta property="og:site_name" content="$domain">
     <meta property="article:author" content="https://manfred.life">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#9f00a7">
+    <meta name="theme-color" content="#ffffff">
   </head>
   <body>
     <h1>$domain/$project_name</h1>
@@ -67,6 +74,13 @@ cat > $rootdir/index.html <<EOF
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://$domain/">
     <meta property="og:image" content="https://$domain/moul-codes.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#9f00a7">
+    <meta name="theme-color" content="#ffffff">
   </head>
   <body>
     <h1>$domain Go Modules</h1>
